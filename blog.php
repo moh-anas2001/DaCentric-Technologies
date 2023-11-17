06d14aeac2d6abac0d2cc9713c6db962dc447807
moh-anas2001 <mohamedanascse@gmail.com>
2023-11-17 10:07:41 +0400

    dmcdk

--- blog.php
+++ blog.php
@@ -15,7 +15,7 @@ $result = $connect->query($sql);
     <meta charset="utf-8">
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-    <title>Bell Bootstrap Template</title>
+    <title>DaCentric Technologies</title>
     <meta content="" name="description">
     <meta content="" name="keywords">
 
