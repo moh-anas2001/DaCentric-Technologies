171722605036f4b956038d65dbebd6b43beee12f
moh-anas2001 <mohamedanascse@gmail.com>
2024-01-09 12:34:19 +0400

    j

--- admin/includes/database.php
+++ admin/includes/database.php
@@ -1,6 +1,6 @@
 <?php
 
-$connect = mysqli_connect('localhost', 'dacentric', 'secret', 'dacentric');
+$connect = mysqli_connect('localhost', 'dacentric', 'secret', 'dacentric');  //Dacentric@db -----  dacenj4b_dacentric
 
 if (mysqli_connect_errno()) {
     exit('Failed to connect to MySQL :' . mysqli_connect_errno());
